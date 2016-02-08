bf80916021b9c969d6697e7896fdf0663bf4c43a
dmarsenault <dmarsenault@gmail.com>
2016-02-08 16:21:36 -0700

    Add date to listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -9,4 +9,14 @@ module Listable
     value = "" if !priority
     return value
   end
+  def format_date (options = {})
+    if options[:start_date]
+      dates = options[:start_date].strftime("%D")
+    elsif options[:end_date]
+      dates << " -- " + end_date.strftime("%D")
+    end
+    !dates = "N/A" if !dates
+    else
+    dates = options[:due] ? options[:due].strftime("%D") : "No due date"
+  end
 end
